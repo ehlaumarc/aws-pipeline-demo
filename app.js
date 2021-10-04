38fe88befd15ff452e3658e740b8263fd3206975
Marc Lau <ehlaumarc.elearning@gmail.com>
2021-10-05 00:10:51 +0800

    Full Pipeline Test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hands On Demo'));
+app.get('/', (req, res) => res.send('Hands On Demo:Required Manual Approval'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
